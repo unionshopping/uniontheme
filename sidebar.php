0926e08975e4e6b3b204525ca8bae866f7661dba
Jim Judd <jim.c.judd@dfinsolutions.com>
2019-04-10 10:27:20 -0500

    initial commit

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+		<div id="sidebar">
+		<!--
+			<?php if( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?>
+			
+			<div class="side_box">
+				
+			</div>
+			
+			<?php endif; ?>
+		-->	
+		</div><!--//sidebar-->
