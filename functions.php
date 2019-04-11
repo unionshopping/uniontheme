b8b8b6dab59153595fdd3de0f48c50387e7e3867
Jim Judd <jim.c.judd@dfinsolutions.com>
2019-04-11 09:43:09 -0500

    added on-hold order canceling

--- functions.php
+++ functions.php
@@ -166,5 +166,31 @@ function bbloomer_save_edit_order( $order_id ) {
         $oldorder->update_status( 'cancelled', 'Order cancelled after editing. New order number: <a href="' . $neworder_edit . '">' . $order_id . '</a> -' );
     }
 }
+
+
+
+//Add order cancel to my account page
+add_filter( 'woocommerce_valid_order_statuses_for_cancel', 'filter_valid_order_statuses_for_cancel', 20, 2 );
+function filter_valid_order_statuses_for_cancel( $statuses, $order = '' ){
+
+    // Set HERE the order statuses where you want the cancel button to appear
+    $custom_statuses    = array( 'on-hold' );
+
+    // Set HERE the delay (in days)
+    $duration = 10; // 3 days
+
+    // UPDATE: Get the order ID and the WC_Order object
+    if( ! is_object( $order ) && isset($_GET['order_id']) )
+        $order = wc_get_order( absint( $_GET['order_id'] ) );
+
+    $delay = $duration*24*60*60; // (duration in seconds)
+    $date_created_time  = strtotime($order->get_date_created()); // Creation date time stamp
+    $date_modified_time = strtotime($order->get_date_modified()); // Modified date time stamp
+    $now = strtotime("now"); // Now  time stamp
+
+    // Using Creation date time stamp
+    if ( ( $date_created_time + $delay ) >= $now ) return $custom_statuses;
+    else return $statuses;
+}
 ?>
 
